9d479576aefdc27d56c7a2cf9b69100ffe2ef132
dottienemeth <37559041+dottienemeth@users.noreply.github.com>
2018-03-19 23:15:04 +0100

    Update example1.js

--- example1.js
+++ example1.js
@@ -1,10 +1,12 @@
-var input1 = [-3, -2, -1, 0, 1, 2, 3, 5, 7];
+
+var input1 = [1, 2, 3, 4, 5, 6];
+var input2 = [-3, -2, -1, 0, 1, 2, 3, 5, 7];
 
 //var myElement = document.getElementById("example1Input");
 //myElement.innerHTML = Average(input1);
 
-alert(Average(input1)); 
-
+alert("Example 1 output: "+Average(input1)); 
+alert("Example 2 output: "+Average(input2)); 
 
 function Average(input){
 
