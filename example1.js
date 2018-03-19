e8fefcf0f1cac4d225f25481f1b2e4e9050b2f06
dottienemeth <37559041+dottienemeth@users.noreply.github.com>
2018-03-19 23:39:43 +0100

    Update example1.js

--- example1.js
+++ example1.js
@@ -8,6 +8,8 @@ var input2 = [-3, -2, -1, 0, 1, 2, 3, 5, 7];
 alert("Example 1 output: "+Average(input1)); 
 alert("Example 2 output: "+Average(input2)); 
 
+alert(Shift("apple"));
+
 function Average(input){
 
 var sum = 0;
@@ -21,6 +23,19 @@ for (i = 0; i < input.length; i++) {
 	}
 }
 
-
 return sum/count;
+}
+
+function Shift(text) {
+
+    text = text.toLowerCase();
+
+    var result = '';
+    var charcode = 0;
+
+    for (var i = 0; i < text.length; i++) {
+        charcode = (text[i].charCodeAt()) + 1;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
 }
